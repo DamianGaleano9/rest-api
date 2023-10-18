8217fa4d089e68df0ac1ec8d51e0292b782cdcc2
Damian Galeano <dj.damian.b@hotmail.com>
2023-10-18 02:08:19 +0200

    Create getSingleUsers

--- src/controllers/users..controllers.js
+++ src/controllers/users..controllers.js
@@ -13,46 +13,16 @@ const getUser = async (req, res) => {
     }
 };
 
+// GET SINGLE USER  
 
-// GET JUST ONE USER 
+const getProduct = async (req, res) => {
 
-
-const getJustOneUser = async (req, res) => {
     try {
+
         console.log(req.params);
-        const { users_id }  = req.params
-        const {connection} = await getConnection();
-        
-        
-        const result = await connection.query("SELECT users_id, users_name FROM users WHERE users_id= ?", users_id);
-        res.json(result);
-    }
-
-
-    catch (err) {
-        res.status(500)
-        res.send(err.message);
-    }
-
-};
-
-
-// ADD USER 
-
-const addUser = async(req, res) => {
-    try {
-
-        const { users_name, users_price, users_description, users_img, users_stock } = req.body;
-
-        if (users_name === undefined || users_price === undefined || users_description === undefined || users_img === undefined || users_stock === undefined) {
-            res.status(400).json({ message: "Bad Request. Please check your request" })
-        }
-
+        const { users_id } = req.params
         const connection = await getConnection();
-        const product = { users_name, users_price, users_description, users_img, users_stock };
-        const result = await connection.query("INSERT INTO users SET ?", product);
-
-
+        const result = await connection.query("SELECT users_id, users_name FROM users WHERE users_id= ?", users_id);
         res.json(result);
 
     } catch (err) {
@@ -60,4 +30,3 @@ const addUser = async(req, res) => {
         res.send(err.message);
     }
 };
-
